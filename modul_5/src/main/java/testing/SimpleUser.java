7f7ca4f906c5c2086cd6453ae506b431fa61b50b
szymonpp2016 <szymon.pp.2016@interia.plgit config --global user.emainameszymon.pp.2016@interia.pl>
2020-05-11 22:38:27 +0200

    First test

---
+++ modul_5/src/main/java/testing/SimpleUser.java
@@ -0,0 +1,14 @@
+package testing;
+
+public class SimpleUser {
+
+    private String userName;
+
+    public SimpleUser(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
--- modul_5/src/main/java/testing/TestingMain.java
+++ modul_5/src/main/java/testing/TestingMain.java
@@ -2,7 +2,19 @@ package testing;
 
 public class TestingMain {
 
+
+    private final String USER_1 = "simple_user";
+    private final String USER_2 = "simple_user_1";
+
     public static void main(String[] args) {
         System.out.println("Do dzieła aaaaa");
+        TestingMain testingMain = new TestingMain();
+        System.out.println(testingMain.sUTShouldReturnCurrentUserReturnIsOk(testingMain.USER_1));
+    }
+
+    private String sUTShouldReturnCurrentUserReturnIsOk(String userName) {
+        SimpleUser simpleUser = new SimpleUser(USER_1);
+        return (simpleUser.getUserName()==USER_1 && simpleUser.getUserName()!=USER_2 ) ? "true" : "false";
     }
 }
+
