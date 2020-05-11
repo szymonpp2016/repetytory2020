b7b042be44de44628484b2219b7f2675e6f7bd95
szymonpp2016 <szymon.pp.2016@interia.plgit config --global user.emainameszymon.pp.2016@interia.pl>
2020-05-11 22:22:58 +0200

    small change

--- modul_5/src/main/java/testing/TestingMain.java
+++ modul_5/src/main/java/testing/TestingMain.java
@@ -3,6 +3,6 @@ package testing;
 public class TestingMain {
 
     public static void main(String[] args) {
-        System.out.println("Do dzieła");
+        System.out.println("Do dzieła aaaaa");
     }
 }
